709dd2fef7c17ff9cb4dc655eb0bbdf81142d1e0
Shachar Oz <shachar.oz@gmail.com>
2019-04-27 18:36:09 +0200

    login & user creation. not safe

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import flask
 import flask_wtf
 import wtforms
+import flask_login
+import secrets
+
 import os
 
 import json_file
@@ -14,8 +17,51 @@ class LoginForm(flask_wtf.FlaskForm):
     submit = wtforms.SubmitField('Login')
 
 
+class User(flask_login.UserMixin):
+
+    @classmethod
+    def gen_id(cls):
+        value = secrets.token_hex(32)
+        print(value)
+        valid = True
+        for name, data in users_json.data.items():
+            print(name, data)
+            if data['id'] == value:
+                valid = False
+                break
+        print(valid)
+        if valid:
+            return value
+        else:
+            return cls.gen_id()
+
+    def register(self, username, password):
+        self.username = username
+        self.password = password
+        users_json.data[self.username] = {'password': password, 'id': self.id}
+        users_json.save()
+
+    def load(self, user_data):
+        self.password = user_data['password']
+        self.id = user_data['id']
+
+    def __init__(self, username, password=None):
+        user_data = users_json.data.get(username)
+        if username in users_json.data:
+            self.load(user_data)
+        else:
+            self.id = self.gen_id()
+            self.register(username, password)
+
+
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'sonobello'
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+users_json = json_file.JsonFile('users.json')
+users_json.load()
 
 slides_json = json_file.JsonFile(os.path.join('static', 'dat/slides.json'))
 slides_json.load()
@@ -39,8 +85,14 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         #flask.flash('Logged in. Not really just a test.', 'success')
+        flask_login.login_user(User(form.username.data, form.password.data))
         return flask.redirect('/slideshow/' + form.username.data + '/1')
     return flask.render_template('login.html', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return None
 
 
 if __name__ == '__main__':
