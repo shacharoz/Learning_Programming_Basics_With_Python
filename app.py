c83f21bf8d2f222ab89fee79744a021d73a54b8f
Shachar Oz <shachar.oz@gmail.com>
2019-05-04 17:29:24 +0200

    user database working

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
+import secrets
+import os
+
 import flask
 import flask_wtf
 import wtforms
 import flask_login
-import secrets
-
-import os
+import flask_sqlalchemy
 
 import json_file
+
+app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'sonobello'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+users_json = json_file.JsonFile('users.json')
+users_json.load()
+
+slides_json = json_file.JsonFile(os.path.join('static', 'dat/slides.json'))
+slides_json.load()
+slides = slides_json.data
 
 
 class LoginForm(flask_wtf.FlaskForm):
@@ -17,55 +35,13 @@ class LoginForm(flask_wtf.FlaskForm):
     submit = wtforms.SubmitField('Login')
 
 
-class User(flask_login.UserMixin):
+class User(flask_login.UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
-    @classmethod
-    def gen_id(cls):
-        value = secrets.token_hex(32)
-        print(value)
-        valid = True
-        for name, data in users_json.data.items():
-            print(name, data)
-            if data['id'] == value:
-                valid = False
-                break
-        print(valid)
-        if valid:
-            return value
-        else:
-            return cls.gen_id()
-
-    def register(self, username, password):
-        self.username = username
-        self.password = password
-        users_json.data[self.username] = {'password': password, 'id': self.id}
-        users_json.save()
-
-    def load(self, user_data):
-        self.password = user_data['password']
-        self.id = user_data['id']
-
-    def __init__(self, username, password=None):
-        user_data = users_json.data.get(username)
-        if username in users_json.data:
-            self.load(user_data)
-        else:
-            self.id = self.gen_id()
-            self.register(username, password)
-
-
-app = flask.Flask(__name__)
-app.config['SECRET_KEY'] = 'sonobello'
-
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-
-users_json = json_file.JsonFile('users.json')
-users_json.load()
-
-slides_json = json_file.JsonFile(os.path.join('static', 'dat/slides.json'))
-slides_json.load()
-slides = slides_json.data
+    def get_id(self):
+        return self.id
 
 
 @app.route('/')
@@ -73,26 +49,36 @@ def index():
     return 'Hello, World! Index page.'
 
 
-@app.route('/slideshow/<string:username>/<int:_index>')
-def slideshow(username, _index):
+@app.route('/slideshow/<int:_index>')
+@flask_login.login_required
+def slideshow(_index):
     slide = slides[_index - 1]
+    user = flask_login.current_user
     return flask.render_template('slideshow.html', title=slide['title'], time=slide['time'], image_path=slide['image'],
-                                 index=_index - 1, back=_index != 1, next=_index < len(slides), username=username)
+                                 index=_index - 1, back=_index != 1, next=_index < len(slides), user=user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        #flask.flash('Logged in. Not really just a test.', 'success')
-        flask_login.login_user(User(form.username.data, form.password.data))
-        return flask.redirect('/slideshow/' + form.username.data + '/1')
+        # flask.flash('Logged in. Not really just a test.', 'success')
+        if db.session.query(
+                db.session.query(User).filter_by(username=form.username.data).exists()
+        ).scalar():
+            flask_login.login_user(User.query.filter_by(username=form.username.data).first())
+        else:
+            user = User(username=form.username.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flask_login.login_user(user)
+        return flask.redirect('/slideshow/1')
     return flask.render_template('login.html', form=form)
 
 
 @login_manager.user_loader
-def load_user(user_id):
-    return None
+def load_user(id):
+    return User.query.filter_by(id=id).first()
 
 
 if __name__ == '__main__':
